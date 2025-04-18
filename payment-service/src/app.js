4b761030889a1b0faf6c642d873bbf1cbb5c6d4c
DJshenal <pererashenal2001@gmail.com>
2025-04-18 18:53:22 +0530

    payment intergrate

--- payment-service/src/app.js
+++ payment-service/src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { stripeWebhook } = require('./controllers/stripeWebhookController');
+
+dotenv.config();
+
 const app = express();
-require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Payment Service');
-});
+app.post('/webhook/stripe', express.raw({ type: 'application/json' }), stripeWebhook);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5006;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(` Payment Service running on port ${PORT}`);
 });
