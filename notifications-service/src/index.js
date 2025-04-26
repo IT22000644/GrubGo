f603d196f3909660cb6bbe6d34b8a5a5a997d185
Manilka Navod <manilka.projects@gmail.com>
2025-04-26 21:46:23 +0530

    updates - sms function

--- notifications-service/src/index.js
+++ notifications-service/src/index.js
@@ -5,9 +5,6 @@ const start = async () => {
   try {
     const channel = await connectQueue();
     const QUEUE_NAME = "notification";
-    await channel.assertQueue(QUEUE_NAME, { durable: true });
-
-    console.log(`[✅] Waiting for messages in ${QUEUE_NAME}`);
     channel.consume(QUEUE_NAME, (msg) => handleNotification(channel, msg));
   } catch (error) {
     console.error("[❌] Error in starting the consumer:", error);
