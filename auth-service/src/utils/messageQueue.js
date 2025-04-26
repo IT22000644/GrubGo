1408e0335e1447cbe05950172ed4205da703f1f7
Manilka Navod <manilka.projects@gmail.com>
2025-04-26 16:20:44 +0530

    payment service kubes - 2

--- auth-service/src/utils/messageQueue.js
+++ auth-service/src/utils/messageQueue.js
@@ -1,11 +1,13 @@
 import amqplib from "amqplib";
-import { RABBIT_MQ } from "../config/index.js";
+import { RABBITMQ_URL } from "../config/index.js";
 
 let channel;
 
 export const connectQueue = async () => {
   try {
-    const connection = await amqplib.connect(RABBIT_MQ || "amqp://localhost");
+    const connection = await amqplib.connect(
+      RABBITMQ_URL || "amqp://localhost"
+    );
     channel = await connection.createChannel();
     console.log("Connected to RabbitMQ");
   } catch (error) {
