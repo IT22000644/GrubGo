f6d30199d5f60c320fd02f2cdc9dcf5f8ffff84b
Manilka Navod <manilka.projects@gmail.com>
2025-04-27 11:15:47 +0530

    login slice

--- FrontEnd/src/features/auth/authSlice.ts
+++ FrontEnd/src/features/auth/authSlice.ts
@@ -1,14 +1,22 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import api from "../../api/api";
 
 interface User {
   id: string;
   name: string;
   email: string;
+  phoneNumber: string;
+  profilePicture: string;
+  role: string;
+  isVerified: boolean;
+  customerDetails: any;
+  riderDetails: any;
 }
 
 interface AuthState {
   isAuthenticated: boolean;
   user: User | null;
+  role: string | null;
   token: string | null;
 }
 
@@ -16,16 +24,27 @@ const initialState: AuthState = {
   isAuthenticated: false,
   user: null,
   token: null,
+  role: null,
 };
 
+export const loginUser = createAsyncThunk<
+  { user: User; token: string },
+  { email: string; password: string }
+>("auth/login", async (formData) => {
+  const response = await api.post("/auth/login", formData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
+  return response.data;
+});
+
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
-    login: (
-      state,
-      action: PayloadAction<{ user: User; token: string }>
-    ) => {
+    login: (state, action: PayloadAction<{ user: User; token: string }>) => {
       state.isAuthenticated = true;
       state.user = action.payload.user;
       state.token = action.payload.token;
@@ -35,6 +54,20 @@ const authSlice = createSlice({
       state.user = null;
       state.token = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.role = action.payload.user.role;
+    });
+    builder.addCase(loginUser.rejected, (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+      state.role = null;
+    });
   },
 });
 
